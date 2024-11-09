98a1da7a48741a0b73fddccdacafc7f25c6942d0
Swati Verma <121037311+codewithswatiee@users.noreply.github.com>
2024-11-09 11:26:39 +0530

    made some changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,34 +3,6 @@ import './App.css'
 import axios from 'axios';
 
 function App() {
-  const [products, setProducts] = useState([]);
-  const [error, setError] = useState(false);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    (async() => {
-      try {
-        setLoading(true);
-        setError(false)
-        const response = await axios.get('/api/products')
-        console.log(response.data)
-        setProducts(response.data);
-        setLoading(false);
-      } catch (error) {
-          setError(true)
-          setLoading(false)
-      }
-    })()
-    
-  }, []);
-
-  if(error){
-    return <div>Error</div>
-  }
-
-  if(loading){
-    return <div>Loading...</div>
-  }
   return (
     <>
       <h1>HI EVERYONE!</h1>
